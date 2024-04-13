5a321f84e110e1dcd7ea951dec3a713c8e102c0b
Your Name <wmsalmanayaj@gmail.com>
2024-04-13 12:14:20 +0530

    new code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,23 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args){
+
+        Product p = new Product();
+        Product ans = new Product();
+        Product ans2 = new Product();
+
+    }
+    public static class Product() {
+
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
 }
